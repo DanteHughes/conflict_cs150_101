aba9c4a07c232adc56c79b58bf3f3d5061058519
Dante Hughes <dhughes@nic.edu>
2024-10-23 10:36:07 -0700

    fixed conflict

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include<iostream>
 using namespace std;
+
+int add(int a, int b);
 int main()
 {
     cout<<"my name is tom"<<endl;
+    cout<<"5+7= "<<add(5,7);
     return 0;
+}
+int add(int a, int b)
+{
+    return a+b;
 }
