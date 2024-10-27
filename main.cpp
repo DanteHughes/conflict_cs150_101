4a4fd3ce18309cf832d242b6e3444898966f57b0
DanteHughes <dhughes@nic.edu>
2024-10-27 18:58:47 +0000

    wrote void function

--- main.cpp
+++ main.cpp
@@ -2,15 +2,21 @@
 using namespace std;
 
 int add(int a, int b);
+void message();
 int main()
 {
 
     cout<<"hello"<<endl;
     cout<<"my name is tom"<<endl;
     cout<<"5+7= "<<add(5,7);
+    message();
     return 0;
 }
 int add(int a, int b)
 {
     return a+b;
+}
+void message()
+{
+    cout<<endl<<"I am inside the function message()"<<endl;
 }
